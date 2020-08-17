da8c6c2c59288b27253eb459da0f8c4d38c292b3
Raisa Primerova <riyuhide@gmail.com>
2020-08-17 16:21:11 -0600

    Update main block class

--- src/views/AuthorizationPage.vue
+++ src/views/AuthorizationPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="welcome-block">
+    <div class="auth-block">
       <div v-if="$route.params.type==='signup'">
         <h2>Create account</h2>
         <SignUp />
@@ -8,6 +8,14 @@
       <div v-if="$route.params.type==='login'">
         <h2>Welcome back</h2>
         <LogIn />
+      </div>
+      <div v-if="$route.params.type==='reset'">
+        <h2>Forgot password?</h2>
+        <ResetPsw />
+      </div>
+      <div v-if="$route.params.type==='new'">
+        <h2>Create new password</h2>
+        <NewPsw />
       </div>
       <img src="../assets/player.svg" alt="player">
     </div>
@@ -17,9 +25,11 @@
 <script>
 import LogIn from '@/components/LogIn';
 import SignUp from '@/components/SignUp';
+import ResetPsw from '@/components/ResetPsw';
+import NewPsw from '@/components/NewPsw';
 
 export default {
   name: 'AuthorizationPage',
-  components: { LogIn, SignUp }
+  components: { LogIn, SignUp, ResetPsw, NewPsw }
 };
 </script>
