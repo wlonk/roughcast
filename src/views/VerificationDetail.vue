46a4816089d735166a7d6f30571fbd19b15ab298
Raisa Primerova <riyuhide@gmail.com>
2020-08-17 17:33:19 -0600

    Create verification page

---
+++ src/views/VerificationDetail.vue
@@ -0,0 +1,35 @@
+<template>
+  <div v-if="currentUser" class="verification-block">
+    <div v-if="currentUser.is_active">
+      <h2>Welcome to Roughcast</h2>
+      <img src="../assets/mailbox.svg" alt="mailbox">
+      <div class="text-block">
+        <p>
+          <router-link :to="`/u/${currentUser.username}`" class="accent-link link">
+            {{ `${currentUser.first_name} ${currentUser.last_name}` }}
+          </router-link>
+          , to start using Roughcast, we need to verify your email. <br>
+          We’ve sent an email with a confirmation link to<br>
+        </p>
+        <p class="accent-link">{{currentUser.email}}</p>
+      </div>
+    </div>
+    <div v-else>
+      <h2>Verified!</h2>
+      <img src="../assets/mailbox-open.svg" alt="mailbox">
+      <p> Great! You have successfully verified your email.</p>
+      <router-link to="/" class="submit-btn">
+        Go to Dashboard!
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'VerificationDetail',
+  computed: mapGetters(['currentUser']),
+};
+</script>
