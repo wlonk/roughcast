89b9a14e9babe5b83368ea6e207b0326098b52b7
Raisa Primerova <riyuhide@gmail.com>
2020-08-17 16:22:26 -0600

    Create reset and change password components

---
+++ src/components/NewPsw.vue
@@ -0,0 +1,33 @@
+<template>
+  <form class="page-form">
+    <div>
+      <label for="password">Password</label>
+      <input
+        name="password"
+        type="password"
+        id="password"
+        placeholder="Password"
+        class="ui input"
+      />
+    </div>
+    <div>
+      <label for="rep-password">Repeat Password</label>
+      <input
+        name="password"
+        type="password"
+        id="rep-password"
+        placeholder="Password"
+        class="ui input"
+      />
+    </div>
+    <div class="submit-row row">
+      <input type="submit" value="Reset password" class="submit-btn" />
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'NewPsw'
+};
+</script>
