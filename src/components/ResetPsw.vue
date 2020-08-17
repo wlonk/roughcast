cd30760f4cb372b5a93ae7141a6de9cdff11b005
Raisa Primerova <riyuhide@gmail.com>
2020-08-17 16:22:26 -0600

    Create reset and change password components

---
+++ src/components/ResetPsw.vue
@@ -0,0 +1,22 @@
+<template>
+  <form class="page-form">
+    <div>
+      <label for="email">Enter your mail</label>
+      <input type="email" name="email" placeholder="example@mail.com" id="email" />
+    </div>
+    <div class="submit-row row">
+      <input type="submit" value="Reset password" class="submit-btn" />
+      <p> Back to
+        <router-link to="/auth/login" class="accent-link">
+          Log In
+        </router-link>
+      </p>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'ResetPsw'
+};
+</script>
