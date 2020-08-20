f259b112fae2c5ab3517cb62fb36cb5ff8b2f610
Raisa Primerova <riyuhide@gmail.com>
2020-08-20 15:56:37 -0600

    Add EditProfilePage view

---
+++ src/views/EditUserPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="edit-block">
+      <h2 >Edit Profile</h2>
+      <div>
+        <ScrollingEditMenu />
+        <div>
+          <div class="edit-box" id="public"></div>
+          <div class="edit-box" id="account"></div>
+          <div class="edit-box" id="password-change"></div>
+          <div class="edit-box" id="notifications"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+import ScrollingEditMenu from '@/components/ScrollingEditMenu';
+
+export default {
+  name: 'EditUserPage',
+  components: {
+    ScrollingEditMenu,
+  },
+  computed: mapGetters(['currentUser']),
+};
+</script>
