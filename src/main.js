1c9abc8b31d472da678aa51520859a28a743356d
Raisa Primerova <riyuhide@gmail.com>
2020-08-20 15:56:00 -0600

    Add scroll-menu package

--- src/main.js
+++ src/main.js
@@ -9,7 +9,9 @@ import 'semantic-ui-css/semantic.min.css';
 import SuiVue from 'semantic-ui-vue';
 import AsyncComputed from 'vue-async-computed';
 import VueAxios from 'vue-axios';
+import VueScrollactive from 'vue-scrollactive';
 
+Vue.use(VueScrollactive);
 Vue.use(SuiVue);
 Vue.use(AsyncComputed);
 Vue.use(VueAxios, api);
