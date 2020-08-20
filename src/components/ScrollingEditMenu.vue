66ffde48e6fdfa7eb69b45f52334a094b2828c22
Raisa Primerova <riyuhide@gmail.com>
2020-08-20 15:57:38 -0600

    Add styled ScrollingEditMenu

---
+++ src/components/ScrollingEditMenu.vue
@@ -0,0 +1,22 @@
+<template>
+  <scrollactive active-class="active" class="my-nav scrolling-menu">
+    <router-link to="#public" class="link scrollactive-item">
+      Public profile
+    </router-link>
+    <router-link to="#account" class="link scrollactive-item">
+      Account settings
+    </router-link>
+    <router-link to="#password-change" class="link scrollactive-item">
+      Change password
+    </router-link>
+    <router-link to="#notifications" class="link scrollactive-item">
+      Notification preferences
+    </router-link>
+  </scrollactive>
+</template>
+
+<script>
+export default {
+  name: 'ScrollingEditMenu'
+};
+</script>
