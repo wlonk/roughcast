f5fb3507efbe42f4e260179f05e6ac01f16916a2
Raisa Primerova <riyuhide@gmail.com>
2020-09-01 21:30:56 -0600

    Recreate structure of game card

--- src/components/GameCard.vue
+++ src/components/GameCard.vue
@@ -1,36 +1,50 @@
 <template>
   <div class="game-card">
     <div>
-      <img v-if="avatar" :src="avatar" alt="no game avatar" />
-      <img v-else src="../assets/no-game-image.svg" alt="no game avatar" />
-      <div class="header">
-        <div class="states">
-          <i class="heart icon"></i>
-          <p class="likes">{{ likes || 0 }}</p>
-          <i class="comment icon"></i>
-          <p class="comments">{{ comments || 0 }}</p>
-          <GameCardMenu v-if="permissions['this:edit']" />
-        </div>
-        <div class="team">
-          <p>{{ team }}</p>
-        </div>
-      </div>
-    </div>
-    <div class="content">
       <div>
+        <img v-if="banner" :src="banner" alt="no game banner" />
+        <img v-else src="../assets/no-game-image.svg" alt="no game banner" />
+      </div>
+      <div class="header">
+        <GameCardMenu v-if="permissions['this:edit']" />
         <router-link :to="`/t/${team.slug}/${slug}`" class="game-card-title">
           {{ name }}
         </router-link>
-        <span>@{{ latest_version.name }}</span>
+        <div class="team">
+          <p>{{ team_name }}</p>
+        </div>
       </div>
-      <div class="description">
-        <RenderedMarkdown :body="description" />
+    </div>
+    <div class="description">
+      <RenderedMarkdown :body="description" />
+    </div>
+    <div class="footer">
+      <div class="states">
+        <i class="heart icon"></i>
+        <p class="likes">{{ likes || 0 }}</p>
+        <i class="comment icon"></i>
+        <p class="comments">{{ comments || 0 }}</p>
+      </div>
+      <div class="authors">
+        <!-- <router-link
+          v-for="(author, index) in authors"
+          :key="index"
+          :to="`/u/${author.username}`">
+          <img
+            :src="item.avatar"
+            alt="author avatar">
+        </router-link> -->
+        <img src="../assets/no-avatar.svg" alt="author avatar">
+        <img src="../assets/no-avatar.svg" alt="author avatar">
+        <img src="../assets/no-avatar.svg" alt="author avatar">
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 import RenderedMarkdown from '@/components/RenderedMarkdown';
 import GameCardMenu from '@/components/GameCardMenu';
 
@@ -38,7 +52,7 @@ export default {
   name: 'GameCard',
   components: { RenderedMarkdown, GameCardMenu },
   props: {
-    avatar: String,
+    banner: String,
     id: String,
     name: String,
     slug: String,
@@ -48,6 +62,13 @@ export default {
     permissions: Object,
     likes: Number,
     comments: Number,
+    authors: Array
+  },
+  computed: {
+    ...mapGetters(['hydratedTeam']),
+    team_name() {
+      return this.hydratedTeam(this.team).name;
+    },
   },
 };
 </script>
