8c6835afc432643987f7387dfb660194b02a5a36
Raisa Primerova <riyuhide@gmail.com>
2020-09-01 20:22:01 -0600

    Add invitation card component and styles

--- src/components/Invitations.vue
+++ src/components/Invitations.vue
@@ -3,6 +3,19 @@
     <div class="box-title">
       <h5>Invitations</h5>
     </div>
+    <div v-if="invitations.length">
+      <InvitationCard
+        v-for="(invite, index) in invitations"
+        :key="index"
+        :sender="invite.sender_username"
+        :team="invite.team"
+        :team_slug="invite.team_slug"
+      />
+    </div>
+    <div v-else>
+      <h6>There is no pending invitation yet.</h6>
+      <p>When somebody invites you, you'll see it here.</p>
+    </div>
   </div>
 </template>
 
@@ -12,6 +25,15 @@ import InvitationCard from '@/components/InvitationCard';
 
 export default {
   name: 'Invitations',
+  data () {
+    return {
+      invitations: [{
+        sender_username: 'mayzee',
+        team: 'Funny Bunnies',
+        team_slug: 'funny-bunny'
+      }]
+    }
+  },
   components: {
     InvitationCard,
   },
