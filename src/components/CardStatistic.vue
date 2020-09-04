aebe7ab914ce81892f66496591c6946abdb2ad57
Raisa Primerova <riyuhide@gmail.com>
2020-09-03 20:48:14 -0600

    Update statistic footer

--- src/components/CardStatistic.vue
+++ src/components/CardStatistic.vue
@@ -1,9 +1,21 @@
 <template>
   <div class="states">
-    <i class="heart icon"></i>
-    <p class="likes">{{ likes || 0 }}</p>
-    <i class="comment icon"></i>
-    <p class="comments">{{ comments || 0 }}</p>
+    <div v-if="likes">
+      <i class="heart icon"></i>
+      <p class="likes">{{ likes }}</p>
+    </div>
+    <div v-if="comments">
+      <i class="comment icon"></i>
+      <p class="comments">{{ comments }}</p>
+    </div>
+    <div v-if="subscribers">
+      <i class="user icon"></i>
+      <p class="subscribers">{{ subscribers }}</p>
+    </div>
+    <div v-if="games">
+      <i class="cube icon"></i>
+      <p class="games">{{ games }}</p>
+    </div>
   </div>
 </template>
 
@@ -11,8 +23,10 @@
 export default {
   name: 'CardStatistic',
   props: {
-    likes: Number,
-    comments: Number,
+    likes: String,
+    comments: String,
+    subscribers: String,
+    games: String
   },
 };
 </script>
