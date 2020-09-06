16d63a42d50b1c54ed57b6549fe7f409d8fc0992
Raisa Primerova <riyuhide@gmail.com>
2020-09-05 22:07:36 -0600

    Update version page

---
+++ src/components/VersionProfileCard.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="version-profile-card">
+    <img v-if="game.banner" :src="game.banner" alt="game banner" />
+    <img v-else src="../assets/no-game-image.svg" alt="no game banner" />
+    <div>
+      <div class="header">
+        <h4 class="card-title">
+          <router-link :to="`/t/${game.team}/${game.slug}`" class="accent-link">
+            {{ game.name }}
+          </router-link>
+          <span>@{{ name }}</span>
+          </h4>
+        <div>
+          <h5>Added</h5>
+          <p>{{ added }}</p>
+          <a :href="archive_link" class="round-link"></a>
+        </div>
+      </div>
+      <div class="team">
+        <router-link :to="`/t/${game.team}`" class="link">
+          {{ team_name }}
+        </router-link>
+      </div>
+      <div>
+        <div class="description">
+          <h5>Changes</h5>
+          <RenderedMarkdown v-if="changelog" :body="changelog" />
+          <p v-else class="no-bio">There is no changes yet...</p>
+        </div>
+        <div class="controls">
+          <router-link
+            :to="`/t/${game.team}/${game.slug}/${slug}/edit`"
+            v-if="permissions['this:edit']"
+            class="accent-link">
+            Edit Version
+          </router-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+import RenderedMarkdown from '@/components/RenderedMarkdown';
+
+export default {
+  name: 'VersionSection',
+  props: {
+    id: String,
+    name: String,
+    team: String,
+    game: Object,
+    gameSlug: String,
+    slug: String,
+    changelog: String,
+    archive_link: String,
+    permissions: Object,
+    attachedFiles: Array,
+    added: {
+      default: '11/08/2020'
+    }
+  },
+  components: { RenderedMarkdown },
+  computed: {
+    ...mapGetters(['hydratedTeam']),
+    team_name() {
+      return this.hydratedTeam(this.game.team).name
+    },
+  }
+};
+</script>
--- src/views/VersionDetail.vue
+++ src/views/VersionDetail.vue
@@ -1,15 +1,66 @@
 <template>
-  <VersionSection v-bind="version" />
+  <VersionProfileCard v-bind="version" />
+  <!-- Files
+  <h4 class="ui horizontal divider">
+    files
+  </h4>
+  <ul>
+    <li v-for="file in version.attachedFiles" :key="file.id">
+      <a :href="file.attached_file">
+        <i class="file pdf outline icon"></i> {{ file.name }}
+      </a>
+    </li>
+  </ul> -->
+  <!-- Comments
+  <h4 class="ui horizontal divider">
+    coming soon: discussion
+  </h4>
+  <div class="ui raised segment">
+    <div class="ui placeholder">
+      <div class="image header">
+        <div class="line"></div>
+        <div class="line"></div>
+      </div>
+      <div class="paragraph">
+        <div class="medium line"></div>
+        <div class="short line"></div>
+      </div>
+    </div>
+  </div>
+  <div class="ui raised segment">
+    <div class="ui placeholder">
+      <div class="image header">
+        <div class="line"></div>
+        <div class="line"></div>
+      </div>
+      <div class="paragraph">
+        <div class="medium line"></div>
+        <div class="short line"></div>
+      </div>
+    </div>
+  </div>
+  <div class="ui raised segment">
+    <div class="ui placeholder">
+      <div class="image header">
+        <div class="line"></div>
+        <div class="line"></div>
+      </div>
+      <div class="paragraph">
+        <div class="medium line"></div>
+        <div class="short line"></div>
+      </div>
+    </div>
+  </div> -->
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
 
-import VersionSection from '@/components/VersionSection';
+import VersionProfileCard from '@/components/VersionProfileCard';
 
 export default {
   name: 'VersionDetail',
-  components: { VersionSection },
+  components: { VersionProfileCard },
   computed: {
     ...mapGetters(['hydratedVersion']),
     version() {
