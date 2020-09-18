25140c9fe7c517dbc291ea0925283baf7a9e7e9c
Kit La Touche <kit@transneptune.net>
2020-09-17 19:17:50 -0600

    Add ToS

---
+++ src/views/TermsOfServicePage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="dashboard">
+    <h1>Terms of Service</h1>
+    <p>
+      This is a sample placeholder terms of service.
+    </p>
+    <p>
+      You represent that everything you upload here is yours to upload. If we
+      get a takedown notice at
+      <a href="mailto:kit+roughcast@transneptune.net"
+        >kit+roughcast@transneptune.net</a
+      >
+      we'll take it down.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TermsOfServicePage',
+};
+</script>
